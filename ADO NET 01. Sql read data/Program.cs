aaf9bd607657e6c9929b47682b2587af4c7b6bdc
Zamanov Nadir Rasim <Zamanov@itstep.edu.az>
2026-09-04 11:29:59 +0400

    ADO 01

---
+++ ADO NET 01. Sql read data/Program.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.Data.SqlClient;
+
+string connectionString = @"Server=(localdb)\MSSQLLocalDB;Database=AdoTest;Integrated Security=True;Trust Server Certificate=True;";
+
+SqlDataReader reader = null;
+SqlCommand cmd = null;
+
+#region Read Data
+
+//using (var connection = new SqlConnection(connectionString))
+//{
+//    cmd = new SqlCommand(@"SELECT * FROM Authors", connection);
+
+//    connection.Open();
+
+//    reader = cmd.ExecuteReader();
+
+//    while (reader.Read())
+//    {
+//        Console.WriteLine($"{reader[0]}. {reader[1]} {reader[2]}");
+//    }
+//}
+#endregion
+
+
+#region Read Data Indexer
+//using (var connection = new SqlConnection(connectionString))
+//{
+//    cmd = new SqlCommand(@"SELECT * FROM Authors", connection);
+
+//    connection.Open();
+
+//    reader = cmd.ExecuteReader();
+
+//    while (reader.Read())
+//    {
+//        Console.WriteLine($"{reader["Id"]}. {reader["FirstName"]} {reader["LastName"]}");
+//    }
+//}
+#endregion
+
+#region FieldCount, .GetName()
+//using (var connection = new SqlConnection(connectionString))
+//{
+//    cmd = new SqlCommand(@"SELECT * FROM Authors", connection);
+
+//    connection.Open();
+
+//    reader = cmd.ExecuteReader();
+//Console.WriteLine(reader.FieldCount);
+//Console.WriteLine(reader.GetName(0));
+//Console.WriteLine(reader.GetName(1));
+//Console.WriteLine(reader.GetName(2));
+
+//    bool line = true;
+
+//    while (reader.Read())
+//    {
+//        if (line)
+//        {
+//            for (int i = 0; i < reader.FieldCount; i++)
+//            {
+//                Console.Write($"{reader.GetName(i)}\t\t\t");
+//            }
+//            Console.WriteLine();
+//            line = false;
+
+//        }
+//        for (int i = 0; i < reader.FieldCount; i++)
+//        {
+//            Console.Write($"{reader[i]}\t\t\t");
+//        }
+//        Console.WriteLine();
+
+//    }
+//}
+#endregion
+
--- ADO NET 01/Program.cs
+++ ADO NET 01/Program.cs
@@ -1,3 +1,97 @@
-﻿/*
- 
+﻿#region ADO Overview
+/*
+ADO .NET - ActiveX Data Object for .NET
+https://learn.microsoft.com/en-us/dotnet/framework/data/adonet/ado-net-overview
+https://en.wikipedia.org/wiki/ADO.NET
+
+MSSQL Server, Oracle, OLE DB, ODBC, ...
+
+ADO Classes:
+    - DbConnection(SqlConnection, ...)
+    - DbCommand
+    - DbDataReader
+    - DbDataAdapter
+    - ...
+
+ADO Datatype for DB
+    - DataTable
+    - DataSet
+    - ...
+
+Connection modes:
+    - Connected Mode
+    - Disconnected Mode
+
+- DB First
+- Code First
+- Model First 
 */
+#endregion
+
+#region Connection String
+// https://www.connectionstrings.com/
+// SQL Server Authentication
+// Server=myServerAddress;Database=myDataBase;User Id=myUsername;Password=myPassword;
+// Data Source=myServerAddress;Initial Catalog=myDataBase;User Id=myUsername;Password=myPassword;
+
+// Windows Authentication
+// Server=myServerAddress;Database=myDataBase;Integrated Security=true;
+// Data Source=myServerAddress;Initial Catalog=myDataBase;Integrated Security=SSPI;
+
+// Data Source=STHQ0124-01;User ID=admin;Password=********;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False;
+#endregion
+
+using Microsoft.Data.SqlClient;
+
+SqlConnection connection = default;
+
+string connectionString = @"Server=STHQ0124-01;
+                            Database=AdoTest;
+                            User ID=admin;
+                            Password=admin;
+                            Trust Server Certificate=True;";
+
+#region DatabaseConnection
+//connection = new SqlConnection(connectionString);
+
+//string insertQuery = """
+//    INSERT INTO Authors (FirstName, LastName)
+//    VALUES('Aleksandr', 'Pushkin')
+//    """;
+
+////SqlCommand command = new(insertQuery, connection);
+
+//SqlCommand command = new();
+//command.Connection = connection;
+//command.CommandText = insertQuery;
+
+//try
+//{
+//    connection.Open();
+//    command.ExecuteNonQuery();
+//}
+//finally
+//{
+//    if (connection is not null) connection.Close();
+//}
+
+
+
+#endregion
+
+#region DatabaseConnection with using
+//using (connection = new(connectionString))
+//{
+//    connection.Open();
+//    string FirstName = Console.ReadLine();
+//    string LastName = Console.ReadLine();
+//    string insertQuery = $"""
+//    INSERT INTO Authors (FirstName, LastName)
+//    VALUES('{FirstName}', '{LastName}')
+//    """;
+//    SqlCommand command = new(insertQuery, connection);
+
+//    command.ExecuteNonQuery();
+//}
+#endregion
+
